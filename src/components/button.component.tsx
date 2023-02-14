d4bf6b5d5985bfe1ddae3ad26749d41c9da96401
cola639 <337507950@qq.com>
2023-02-14 11:29:12 +0800

    add

---
+++ src/components/button.component.tsx
@@ -0,0 +1,24 @@
+import {
+  Button as MuiButton,
+  ButtonProps as MuiButtonProps,
+} from "@mui/material"
+
+// Only include variant, size, color from MuiButtonProps
+type ButtonBaseProps = Pick<MuiButtonProps, "variant" | "size" | "color">
+
+// Include everything from MuiButtonProps except disableRipple
+// type ButtonBaseProps = Omit<MuiButtonProps, "disableRipple">;
+
+export interface ButtonProps extends ButtonBaseProps {
+  label: string
+}
+
+export const Button = ({ label, ...rest }: ButtonProps) => (
+  <MuiButton {...rest}>{label}</MuiButton>
+)
+
+Button.defaultProps = {
+  variant: "contained",
+  size: "medium",
+  color: "primary",
+}
